380921cd35d05127f6fa15b05420689be6aff73b
Megs <Megs.close@gmail.com>
2021-02-13 14:32:15 -0800

    made arrays with chosen parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,79 @@
 // Assignment code here
+const specialChar = ["!", "-", "_", "?", "/", "@", "#", "$", "&", "*", ";", "=", "+", "%"];
+
+const lowerAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+const upperCaseAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var userChoice = [];
+
+
+
+function getLength() {
+  var lengthOf = parseInt(window.prompt("How many characters long would you like password to be? Must be at least 8 and no more than 128."));
+  if (lengthOf >= 8 && lengthOf <= 128) {
+    return lengthOf;
+  } else {
+    window.alert("Your choice must be no less than 8 and no more than 128.");
+  getLength();
+  }
+};
+
+function passwordArray() {
+  var specialCharConfirm = window.confirm("Would you like your password to contain special characters?");
+  if (specialCharConfirm) {
+    (userChoice) = (specialChar);
+  } else (userChoice) = (userChoice);
+
+  var lowerAlphConfirm = window.confirm("Would you like your password to contain lowercase letters?");
+  if (lowerAlphConfirm) {
+    (userChoice) = userChoice.concat(lowerAlph);
+  } else (userChoice) = (userChoice);
+
+  var upperCaseConfirm = window.confirm("Would you like your password to contain uppercase letters?");
+  if (upperCaseConfirm) {
+    (userChoice) = userChoice.concat(upperCaseAlph);
+  } else (userChoice) = (userChoice);
+
+  var numbersConfirm = window.confirm("Would you like your password to contain numbers?");
+  if (numbersConfirm) {
+    (userChoice) = userChoice.concat(numbers);
+  } else (userChoice) = (userChoice);
+
+  if(userChoice.length <1) {
+    window.alert("You much choose at least one option to create a password.")
+    passwordArray()
+  }
+};
+
+
+
+getLength();
+
+passwordArray();
+
+console.log(userChoice);
+//var lengthOf = parseInt(window.prompt("How many characters long would you like password to be? Must be at least 8 and no more than 128."));
+
+
+//while (lengthOf < 8 || lengthOf > 128 || "") {
+  
+//  window.prompt("Your choice must be no less than 8 and no more than 128.");
+//  break;
+//}
+
+
+
+//var specialCharConfirm = window.confirm("Would you like your password to contain special characters?")
+
+//var lowerAlphConfirm = window.confirm("Would you like your password to contain lowercase letters?")
+
+//var upperCaseConfirm = window.confirm("Would you like your password to contain uppercase letters?")
+
+//var numbersConfirm = window.confirm("Would you like your password to contain numbers?")
+
 
 
 // Get references to the #generate element
